9bf5b6f407a8c2234a224cd1a1a64d9fc02e2cc1
Aditya-Prakash14 <ap312038@gmail.com>
2025-11-07 14:25:26 +0530

    remove unwanted data

--- tourtrack/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as GradientButton } from './GradientButton';
-export { default as MapWidget } from './MapWidget';
-export { default as GlassCard } from './GlassCard';
-export { default as PulseIcon } from './PulseIcon';
-export { default as FloatingElements } from './FloatingElements';
